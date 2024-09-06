e1c072fa362f1b8a32d549f1c90aa36f2caff1ec
DockerT <DockerT>
2024-09-06 13:41:07 +0530

    Kunal working

--- expressapp/app.js
+++ expressapp/app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
   // Using send function we send
   // response to the client
   // Here we are sending html
-  res.send("<h1> Hello World yo</h1>");
+  res.send("<h1>Hello form kunal</h1>");
 });
 
 // It configures the system to listen
